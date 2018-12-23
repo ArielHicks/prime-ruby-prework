03fb7b684cbffc26606f27390508db53feb2870a
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-23 18:05:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,5 @@
 # Add  code here!
-def prime?(integer)
-  prime?.each(100) do |prime|
+def prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
 end
