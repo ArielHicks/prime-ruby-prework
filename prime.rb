cc09c0594a23648e5f7ff84e6842482662614cf3
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-23 18:07:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,4 @@
-# Add  code here!
 def prime?(num)
-  (2..Math.sqrt(num)).each { |i|
-  if num % i == 0 && i < num
-    return false
-  end
-  }
-  true
-  end
+   Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
+   true
+end
